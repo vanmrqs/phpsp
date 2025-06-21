b358e74fb0e454a6a4bb2f8f45985f7fe87e331f
Vanessa Marques <vanmrqs@outlook.com.br>
2025-06-21 14:02:25 -0300

    Corrigida conexão com banco de dados

--- app/Conexao.php
+++ app/Conexao.php
@@ -11,10 +11,10 @@ class Conexao {
     public static function conectar() {
         if ( ! isset(self::$pdo) ) {
             try {
-                $db_host = getenv('DB_HOST');
-                $db_name = getenv('DB_NAME');
-                $db_user = getenv('DB_USER');
-                $db_pass = getenv('DB_PASS');
+                $db_host = $_ENV['DB_HOST'];
+                $db_name = $_ENV['DB_NAME'];
+                $db_user = $_ENV['DB_USER'];
+                $db_pass = $_ENV['DB_PASS'];
 
                 self::$pdo = new PDO("mysql:host={$db_host};dbname={$db_name};charset=utf8", $db_user, $db_pass);
                 self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
